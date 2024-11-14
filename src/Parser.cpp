5e6af27cdb48f496f1b78579bcc691c4cd098968
Nathan Aquino <nbaquino02@gmail.com>
2024-11-14 22:49:32 +0800

    ineme yung parser

--- src/Parser.cpp
+++ src/Parser.cpp
@@ -1,12 +1,45 @@
 #include <iostream>
 #include <vector>
-#include <Scanner.h>
+#include "../include/Scanner.h"
+
+//Class for Sentence
+
+
+//Class for Atomic Sentence
+
+
+//Class for Complex Sentence
+
+
+//Class for Negated Sentence
+
 
 class Parser {
 private:
-    std::vector<Token> tokens;
+    const std::vector<Token>& tokens;
     int current = 0;
 
 public:
-    Parser(std::vector<Token> tokens) : tokens(tokens) {}
+    /*
+     * Constructor for the Parser class
+     * @param tokens
+     * The vector of tokens to be parsed
+     */
+    Parser(const std::vector<Token>& tokens) : tokens(tokens) {}
+
+    /*
+     * Checks if the current token matches the given type and advances if it does
+     * @param type
+     * The token type to match against
+     * @return
+     * True if the current token matches the given type (and advances), false otherwise
+     */
+    bool match(TokenType type) {
+        if (current >= tokens.size()) return false;  // This check is important to prevent accessing tokens out of bounds
+        if (tokens[current].type == type) {
+            current++;
+            return true;
+        }
+        return false;
+    }
 };
